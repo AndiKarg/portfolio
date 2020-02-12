b29a306d42037a5e6d28b00a4c5c1d4defcde8d7
xProvocation <karg.andi@t-online.de>
2020-02-12 13:40:50 +0100

    Some Changes

--- src/components/smartphone/Smartphone.js
+++ src/components/smartphone/Smartphone.js
@@ -9,6 +9,7 @@ import { CardBack, Card } from './Cards'
 import './smartphone.css'
 import Bg from "./whatsappbg.png"
 import Notch from "./iphone-x-notch.png"
+import Profilepic from "./unsplash.jpg"
 
 const OFFSET = 90
 const SLOW = config.gentle
@@ -18,20 +19,19 @@ export default function App() {
   const [{ y }, set] = useSpring(() => ({ y: OFFSET }))
   const bind = useGesture(({ delta: [, y], down }) => set({ y: y > 400 ? 780 : !down ? OFFSET : y + OFFSET, config: !down || y > 400 ? SLOW : FAST }))
   const opacity = y.interpolate([180, 400], [0.2, 1], 'clamp')
-  const transform = y.interpolate([OFFSET, 250], [40, 0], 'clamp').interpolate(val => `translate3d(0,${val}px,0)`)
+  const transform = y.interpolate([OFFSET, 550], [100, 0], 'clamp').interpolate(val => `translate3d(0,${val}px,0)`)
   return (
     <Device>
       <div className="statusbar" style={{ backgroundImage: 'url(' + Notch + ')' }} />
       <CardBack onClick={() => set({ y: OFFSET })} style={{ opacity, transform, backgroundImage: 'url(' + Bg + ')' }}>
-        <div className="statusbar" style={{ background: 'grey' }} />
         <div className="chathead">
           <div className="back">
             <FontAwesomeIcon icon={faArrowLeft} />
           </div>
-          <div className="name">Name</div>
-          <div className="extra">Telefon und Pic</div>
+          <div className="name">Andreas Karg</div>
+          <div className="extra" style={{ backgroundImage: 'url('+Profilepic+')' }}></div>
         </div>
-        <div className="mainmessage">Sers</div>
+        <div className="mainmessage">Schee dasd do bist &#128514; </div>
         <div className="sendcont">
           <div className="inputcont">
             <div className="inputmessage">Hier Nachricht eingeben</div>
